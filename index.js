c800a7b3cf0d284589be3a6b8797543d77ebcab6
Atandagideon <86412882+Atandagideon@users.noreply.github.com>
2023-02-17 10:29:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+function openSlide() {
+  document.getElementById("myNav").style.width = "80%";
+}
+
+function closeSlide() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openSlide1() {
+  document.getElementById("myNav").style.width = "80%";
+}
+
+function closeSlide1() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openSlide2() {
+  document.getElementById("myNav").style.width = "80%";
+}
+
+function closeSlide2() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openSlide3() {
+  document.getElementById("myNav").style.width = "80%";
+}
+
+function closeSlide3() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+function openSlide4() {
+  document.getElementById("myNav").style.width = "80%";
+}
+
+function closeSlide4() {
+  document.getElementById("myNav").style.width = "0%";
+}
